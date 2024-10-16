0c17c20b43860a83d2a71c9444bda617fb851aae
andresdanielmtz <autummata@gmail.com>
2024-10-16 10:18:21 -0600

    Update: Env Variables, removed "VITE" prefix which came from Frontend implementation

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def igdb_proxy():
     conn = http.client.HTTPSConnection("api.igdb.com")
     payload = "fields name,cover.*,summary;\nwhere category = 0 & platforms = 48;\nsort rating desc;\nlimit 30;"
     headers = {
-        'Client-ID': os.getenv('VITE_CLIENT_ID'),
-        'Authorization': f"Bearer {os.getenv('VITE_ACCESS_TOKEN')}",
+        'Client-ID': os.getenv('CLIENT_ID'),
+        'Authorization': f"Bearer {os.getenv('ACCESS_TOKEN')}",
         'Content-Type': 'text/plain'
     }
     conn.request("POST", "/v4/games", payload, headers)
@@ -37,6 +37,11 @@ def igdb_proxy():
             game['cover']['url'] = game['cover']['url'].replace('t_thumb', 't_cover_big')
     
     return json.dumps(games)
+
+# TODO: Accounts and authentication :)  
+@app.route("/add-game", methods=['POST'])
+def add_game():
+    pass 
 
 if __name__ == "__main__":
     load_dotenv()
